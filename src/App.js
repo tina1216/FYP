25be2d7fcf53da043944498368ca9291bc179a12
tina1216 <takako.ballet.1216@gmail.com>
2023-10-25 02:30:18 +0800

    roughly done css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import Login from "./pages/Login";
+import Select01 from "./pages/Select01";
+import Select02 from "./pages/Select02";
+import Select03 from "./pages/Select03";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/select01" element={<Select01 />} />
+        <Route path="/select02" element={<Select02 />} />
+        <Route path="/select03" element={<Select03 />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
---
+++ src/pages/Select03.js
@@ -0,0 +1,65 @@
+export default function Select03() {
+  return (
+    <>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="gap-6 flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <h1 className="text-xl font-sans font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            辞めさせたい意思がある場合は選択してください。
+          </h1>
+
+          <div className="w-full md:mt-0 sm:max-w-md xl:p-0">
+            <div className="mb-4 py-2 p-6 space-y-4 bg-white rounded-lg shadow dark:border md:space-y-6 sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+              <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                <li className="py-3 sm:py-4">
+                  <a href="/" className="flex items-center space-x-4">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src="../assets/image/photo01.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-md font-sans text-gray-900 truncate dark:text-white">
+                        裁判官の名前
+                      </p>
+                    </div>
+                  </a>
+                </li>
+                <li className="py-3 sm:py-4">
+                  <a href="/" className="flex items-center space-x-4">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src="../assets/image/photo01.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-md font-sans text-gray-900 truncate dark:text-white">
+                        裁判官の名前
+                      </p>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full font-sans text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              次へ
+            </button>
+            <button
+              type="submit"
+              className="w-full mt-4 font-sans text-primary-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 "
+            >
+              戻る
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
