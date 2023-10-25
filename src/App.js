72405eb418f6a00230f513d859cc07eda5f8a4c8
tina1216 <takako.ballet.1216@gmail.com>
2023-10-25 16:13:01 +0800

    comfirm ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from "./pages/Login";
 import Select01 from "./pages/Select01";
 import Select02 from "./pages/Select02";
 import Select03 from "./pages/Select03";
+import Comfirm from "./pages/Comfirm";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/select01" element={<Select01 />} />
         <Route path="/select02" element={<Select02 />} />
         <Route path="/select03" element={<Select03 />} />
+        <Route path="/comfirm" element={<Comfirm />} />
       </Routes>
     </>
   );
